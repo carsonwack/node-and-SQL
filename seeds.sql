91d2338532e7b9a1d3bda58d7e4dd04b14cea3b7
Carson Wack <carson_wack@yahoo.com>
2019-01-12 18:50:13 -0800

    finishing touches

--- seeds.sql
+++ seeds.sql
@@ -1,35 +1,33 @@
 
--- insert into products (product_name, department_name, price, stock_quantity)
--- values("Pyro Flask", "Misc", 45.00, 100); 
+insert into products (product_name, department_name, price, stock_quantity)
+values("Pyro Flask", "Misc", 45.00, 100); 
 
--- insert into products (product_name, department_name, price, stock_quantity)
--- values("Nondescript Ball", "Sports", 5.00, 100); 
+insert into products (product_name, department_name, price, stock_quantity)
+values("Nondescript Ball", "Sports", 5.00, 100); 
 
--- insert into products (product_name, department_name, price, stock_quantity)
--- values("Creepy Doll", "Everyday", 15.00, 100); 
+insert into products (product_name, department_name, price, stock_quantity)
+values("Creepy Doll", "Everyday", 15.00, 100); 
 
--- insert into products (product_name, department_name, price, stock_quantity)
--- values("Stick", "Misc", 4.50, 100); 
+insert into products (product_name, department_name, price, stock_quantity)
+values("Stick", "Misc", 4.50, 100); 
 
--- insert into products (product_name, department_name, price, stock_quantity)
--- values("Fake Necklace", "Fake Jewelry", 6.00, 100); 
+insert into products (product_name, department_name, price, stock_quantity)
+values("Fake Necklace", "Fake Jewelry", 6.00, 100); 
 
--- insert into products (product_name, department_name, price, stock_quantity)
--- values("Invisible Laser Pointer", "Electronics", 2.99, 100); 
+insert into products (product_name, department_name, price, stock_quantity)
+values("Invisible Laser Pointer", "Electronics", 2.99, 100); 
 
--- insert into products (product_name, department_name, price, stock_quantity)
--- values("Wikipedia Poster", "Misc", 12.00, 100);
+insert into products (product_name, department_name, price, stock_quantity)
+values("Wikipedia Poster", "Misc", 12.00, 100);
 
--- insert into products (product_name, department_name, price, stock_quantity)
--- values("FirePods", "Electronics", 299.99, 100); 
+insert into products (product_name, department_name, price, stock_quantity)
+values("FirePods", "Electronics", 299.99, 100); 
 
--- insert into products (product_name, department_name, price, stock_quantity)
--- values("Robot Frog", "Electronics", 199.99, 100); 
+insert into products (product_name, department_name, price, stock_quantity)
+values("Robot Frog", "Electronics", 199.99, 100); 
 
--- insert into products (product_name, department_name, price, stock_quantity)
--- values("Cricket Bat", "Sports", 40.00, 100); 
-
-
+insert into products (product_name, department_name, price, stock_quantity)
+values("Cricket Bat", "Sports", 40.00, 100); 
 
 
 
@@ -37,5 +35,13 @@
 
 
 
+
+
+
+-- select * from products where product_name = "Pyro Flask";
+
+-- update products set stock_quantity = stock_quantity - 1 where item_id = 1;
 
 select * from products;
+
+-- select price from products where item_id = 6;
